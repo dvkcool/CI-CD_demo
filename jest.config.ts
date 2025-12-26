84cebee366c387dc9ae2305b5f77f506b2ff374e
divaynshu <divyanshuk@cdac.in>
2025-12-26 17:49:40 +0530

    fixing jest

--- jest.config.ts
+++ jest.config.ts
@@ -155,7 +155,6 @@ const config: Config = {
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  testEnvironment: "jsdom",
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
 
