7626278a4c8a57c0d487c708f04a18287ed7e5c9
divaynshu <divyanshuk@cdac.in>
2025-12-26 17:42:33 +0530

    Fixing tests and build

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,12 @@
  */
 
 import type {Config} from 'jest';
+import nextJest from 'next/jest.js'
+ 
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './',
+})
 
 const config: Config = {
   // All imported modules in your tests should be mocked automatically
@@ -14,7 +20,8 @@ const config: Config = {
 
   // The directory where Jest should store its cached dependency information
   // cacheDirectory: "/tmp/jest_rs",
-
+  coverageProvider: 'v8',
+  testEnvironment: 'jsdom',
   // Automatically clear mock calls, instances, contexts and results before every test
   clearMocks: true,
 
@@ -33,7 +40,7 @@ const config: Config = {
   // ],
 
   // Indicates which provider should be used to instrument code for coverage
-  coverageProvider: "v8",
+  // coverageProvider: "babel",
 
   // A list of reporter names that Jest uses when writing coverage reports
   // coverageReporters: [
@@ -198,4 +205,4 @@ const config: Config = {
   // watchman: true,
 };
 
-export default config;
+export default createJestConfig(config);
